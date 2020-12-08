c1b59255155c363a13aeb9f7d7b5430559e490d4
h3ndr4kw <h3ndr4kw@gmail.com>
2020-12-08 22:33:09 +0700

    initial

---
+++ boolean.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    bool isCodingFun = true;
+    bool isFishTasty = false;
+
+    std::cout << isCodingFun << std::endl; //Output 1 (true)
+    std::cout << isFishTasty << std::endl; //Input 0 (false) 
+
+    return 0;
+}
